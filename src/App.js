54615fca691144b9cbc8c17742212961bc82c8d3
Ashish Ranjan <akp9801@gmail.com>
2022-12-29 19:20:14 +0530

    props

--- src/App.js
+++ src/App.js
@@ -1,16 +1,10 @@
+import Myform from "./form";
+
 function App(){
   return(
     <div>
-    <h2>Queries</h2>
-    <form>
-      <input type="text" name="name" placeholder="Enter Name" />
-      <br />
-      <input type="text" name="mobile" placeholder="Mobile No." />
-      <br />
-      <input type="password" name="pass" placeholder="Enter Password" />
-      <br />
-      <input type="submit" />
-    </form>
+    <h2>Queries Form</h2>
+    <Myform />
     </div>
   )
 }
